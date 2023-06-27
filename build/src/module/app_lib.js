3c033479213cd6a1c57c29eaad2b65bab4c86e5c
buraya.yuliya <burayayulya374@gmail.com>
2023-06-27 21:54:49 +0300

    Singlton pattern

--- build/src/module/app_lib.js
+++ build/src/module/app_lib.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const really_relaxed_json_1 = require("really-relaxed-json");
 const json = '["one", "two", "three", {"foo": "bar"}]';
-const rjson = (0, really_relaxed_json_1.toRJson)(json);
+const rjson = (0, really_relaxed_json_1.toJson)(json);
 console.log(json);
--- src/factory.ts
+++ src/factory.ts
@@ -1,69 +1,65 @@
 //1 способ
-interface IInsurance{
- id:number;
- status:string;
- setVehicle(vehicle:any):void;
- submit():Promise<boolean>;
+interface IInsurance {
+  id: number;
+  status: string;
+  setVehicle(vehicle: any): void;
+  submit(): Promise<boolean>;
 }
 
-class TFInsurance implements IInsurance{
-    id: number;
-    status: string;
-    private vehicle:any;
-    setVehicle(vehicle: any): void {
-       this.vehicle = vehicle;
-    }
-    async submit(): Promise<boolean> {
-        const res = await fetch('',
-         {
-        method:'POST', 
-        body:JSON.stringify({vehicle:this.vehicle})
+class TFInsurance implements IInsurance {
+  id: number;
+  status: string;
+  private vehicle: any;
+  setVehicle(vehicle: any): void {
+    this.vehicle = vehicle;
+  }
+  async submit(): Promise<boolean> {
+    const res = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({ vehicle: this.vehicle }),
     });
     const data = await res.json();
     return data.isSuccess;
-}
+  }
 }
 
-class ABInsurance implements IInsurance{
-    id: number;
-    status: string;
-    private vehicle:any;
-    setVehicle(vehicle: any): void {
-       this.vehicle = vehicle;
-    }
-    async submit(): Promise<boolean> {
-        const res = await fetch('ab',
-         {
-        method:'POST', 
-        body:JSON.stringify({vehicle:this.vehicle})
+class ABInsurance implements IInsurance {
+  id: number;
+  status: string;
+  private vehicle: any;
+  setVehicle(vehicle: any): void {
+    this.vehicle = vehicle;
+  }
+  async submit(): Promise<boolean> {
+    const res = await fetch("ab", {
+      method: "POST",
+      body: JSON.stringify({ vehicle: this.vehicle }),
     });
     const data = await res.json();
     return data.yes;
-}
+  }
 }
 
 abstract class InsuranceFactory {
-    db:any; 
+  db: any;
 
-    abstract createInsurance(): IInsurance;
+  abstract createInsurance(): IInsurance;
 
-    saveHistory(ins:IInsurance){
-        this.db.save(ins.id, ins.status)
-    }
+  saveHistory(ins: IInsurance) {
+    this.db.save(ins.id, ins.status);
+  }
 }
 
-class TFInsuranceFactory extends InsuranceFactory{
-    createInsurance(): TFInsurance {
-        return new TFInsurance();
-    }
-
+class TFInsuranceFactory extends InsuranceFactory {
+  createInsurance(): TFInsurance {
+    return new TFInsurance();
+  }
 }
 
-class ABInsuranceFactory extends InsuranceFactory{
-    createInsurance(): ABInsurance {
-        return new ABInsurance();
-    }
-
+class ABInsuranceFactory extends InsuranceFactory {
+  createInsurance(): ABInsurance {
+    return new ABInsurance();
+  }
 }
 
 const tfInsuranceFactory = new TFInsuranceFactory();
@@ -73,22 +69,21 @@ tfInsuranceFactory.saveHistory(ins);
 //2 способ
 const INSURANCE_TYPE = {
   tf: TFInsurance,
-  ab:ABInsurance  
+  ab: ABInsurance,
 };
 
 type InsT = typeof INSURANCE_TYPE;
 
-class InsuranceFactoryAlt{
-    db:any;
-    createInsurance<T extends keyof InsT>(type:T): InsT[T]{
-     return INSURANCE_TYPE[type];
-    }
+class InsuranceFactoryAlt {
+  db: any;
+  createInsurance<T extends keyof InsT>(type: T): InsT[T] {
+    return INSURANCE_TYPE[type];
+  }
 
-    saveHistory(ins:IInsurance){
-        this.db.save(ins.id, ins.status)
-    } 
-} 
-   const insuranceAltFactory = new InsuranceFactoryAlt();
-   const ins2 = new  (insuranceAltFactory.createInsurance('tf'));
-   insuranceAltFactory.saveHistory(ins2);
-
+  saveHistory(ins: IInsurance) {
+    this.db.save(ins.id, ins.status);
+  }
+}
+const insuranceAltFactory = new InsuranceFactoryAlt();
+const ins2 = new (insuranceAltFactory.createInsurance("tf"))();
+insuranceAltFactory.saveHistory(ins2);
